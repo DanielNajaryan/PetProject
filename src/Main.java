e07913c751d3d43a202644b0a94183467973505c
maane2 <evrmail2@gmail.com>
2026-03-10 12:09:34 +0400

    translating some instructions to armenian

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,9 @@ public class Main {
         System.out.println("welcome to Kimixito-s calculator\n");
         while (true) {
             System.out.println(
-                               "Choose input method:\n" +
-                               "1 - Input matrix manually\n" +
-                               "2 - Load matrices from JSON file");
+                    "Choose input method:\n" +
+                            "1 - Input matrix manually\n" +
+                            "2 - Load matrices from JSON file");
             if (scanner.hasNextInt()) {
                 inputType = scanner.nextInt();
 
@@ -40,56 +40,56 @@ public class Main {
         }
         solveMatrix(matrix, scanner);
     }
-        public static void solveMatrix ( double[][] arr, Scanner scanner) {
-            int i;
-            System.out.println("input 1 if you want decide your extended matrix with\n" +
-                    "gaus method else input 2 if you want decide your extended matrix with jacobi method");
-            while (true) {
-                if (!scanner.hasNextInt()) {
-                    System.out.println("Please input only number 1 or 2");
-                    scanner.next();
-                    continue;
-                }
-                i = scanner.nextInt();
-                if (i == 1 || i == 2) {
-                    break;
-                } else {
-                    System.out.println("your input is wrong, input 1 or 2 as stated in instruction");
-                }
+    public static void solveMatrix ( double[][] arr, Scanner scanner) {
+        int i;
+        System.out.println("input 1 if you want decide your extended matrix with\n" +
+                "gaus method else input 2 if you want decide your extended matrix with jacobi method");
+        while (true) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please input only number 1 or 2");
+                scanner.next();
+                continue;
             }
-            System.out.println("and say if u want it to be saved it in some file if yes input 1 if no input 2");
-            int whrinfile;
-            while (true) {
-                if (!scanner.hasNextInt()) {
-                    System.out.println("Please input 1 or 2");
-                    scanner.next();
-                    continue;
-                }
-                whrinfile = scanner.nextInt();
-                if (whrinfile == 1 || whrinfile == 2) {
-                    break;
-                } else {
-                    System.out.println("Please input only 1 or 2");
-                }
-            }
-            if (i == 1) {
-                gaus_method solver = new gaus_method();
-                solver.setMatrix(arr);
-                double[] solution = solver.getvalues();
-                solver.etveradardzum(solution);
-                if (whrinfile == 1) {
-                    whriteresult.writeResultToFile(solution);
-                }
-            } else if (i == 2) {
-                jacobi_method jm = new jacobi_method();
-                jm.setMatrix(arr);
-                jm.askforsteps();
-                double[] res = jm.getvalues();
-                jm.etveradardzum(res);
-                if (whrinfile == 1) {
-                    whriteresult.writeResultToFile(res);
-                }
+            i = scanner.nextInt();
+            if (i == 1 || i == 2) {
+                break;
+            } else {
+                System.out.println("your input is wrong, input 1 or 2 as stated in instruction");
             }
         }
+        System.out.println("and say if u want it to be saved it in some file if yes input 1 if no input 2");
+        int whrinfile;
+        while (true) {
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please input 1 or 2");
+                scanner.next();
+                continue;
+            }
+            whrinfile = scanner.nextInt();
+            if (whrinfile == 1 || whrinfile == 2) {
+                break;
+            } else {
+                System.out.println("Please input only 1 or 2");
+            }
+        }
+        if (i == 1) {
+            gaus_method solver = new gaus_method();
+            solver.setMatrix(arr);
+            double[] solution = solver.getvalues();
+            solver.etveradardzum(solution);
+            if (whrinfile == 1) {
+                whriteresult.writeResultToFile(solution);
+            }
+        } else if (i == 2) {
+            jacobi_method jm = new jacobi_method();
+            jm.setMatrix(arr);
+            jm.askforsteps();
+            double[] res = jm.getvalues();
+            jm.etveradardzum(res);
+            if (whrinfile == 1) {
+                whriteresult.writeResultToFile(res);
+            }
+        }
+    }
 
 }
--- src/petprogj/ImpMatrix.java
+++ src/petprogj/ImpMatrix.java
@@ -47,7 +47,7 @@ public class ImpMatrix {
         return arr;
     }
     public void setMaxgcivra(){
-    double[] maxgcivra =new double[x];
+        double[] maxgcivra =new double[x];
         for (int i = 0; i < x; i++) {
             double max = arr[i][0];
             for (int j = 1; j < x + 1; j++) {
