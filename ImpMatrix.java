e788d679f5462835d075a2b798014fa4de27e38e
DanielNajaryan <danielnajaryanjb@gmail.com>
2026-03-10 11:54:22 +0400

    Add files via upload

---
+++ ImpMatrix.java
@@ -0,0 +1,61 @@
+package petprogj;
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ImpMatrix {
+    private double[][] arr;
+    private int x,u,n,l;
+    int [] maxtver;
+    Scanner scanner= new Scanner(System .in);
+    //   x-y et matricai chapna lenght and weight
+    public double[][] matrixint() {
+        while (true) {
+            System.out.print("Գրեք մատրիցի երկարությունը: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Please enter a number!");
+                scanner.next();
+                continue;
+            }
+            x = scanner.nextInt();
+            if (x <= 1) {
+                System.out.println("matrix's length never can be less than 1");
+                continue;
+            }
+            break;
+        }
+        arr = new double[x][x + 1];
+        System.out.println("Գրեք մատրիցի մեջ եղած թվերը, յուրաքնչյուր թվի ներմուծումից հետո սեղմեք enter");
+        for (int i = 0; i < x; i++) {
+            for (int k = 0; k < x + 1; k++) {
+                while (!scanner.hasNextDouble()) {
+                    System.out.println("Please enter a number:");
+                    scanner.next();
+                }
+                arr[i][k] = scanner.nextDouble();
+            }
+        }
+        System.out.println("matrix");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x + 1; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        return arr;
+    }
+    public void setMaxgcivra(){
+    double[] maxgcivra =new double[x];
+        for (int i = 0; i < x; i++) {
+            double max = arr[i][0];
+            for (int j = 1; j < x + 1; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+            maxgcivra[i] = max;
+        }
+    }
+}
---
+++ jacobi_method.java
@@ -0,0 +1,91 @@
+package petprogj;
+import java.util.Scanner;
+
+public class jacobi_method {
+    Scanner sc = new Scanner(System.in);
+    private int ans;
+    private double[][] a;
+    private int matricai_qanak;
+    private int x, y, u;
+    private double eps = 1e-6;
+    private int priblijenyaneriqanaky;
+    private boolean showSteps;
+
+    public void setMatrix(double[][] matrix) {
+        this.a = matrix;
+        this.matricai_qanak = matrix.length;
+    }
+    public void askforsteps() {
+        while (true) {
+            System.out.print("Ցույց տալ ճշգրտությունը" + "? գրեք 1 եթե այո | 2 եթե ոչ/false: ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Please input 1 or 2");
+                sc.next();
+                continue;
+            }
+            ans = sc.nextInt();
+            if (ans == 1 || ans == 2) {
+                if (ans == 1) {
+                    showSteps = true;
+                } else {
+                    showSteps = false;
+                }
+                break;
+            } else {
+                System.out.println("Input must be 1 or 2");
+            }
+        }
+    }
+    public double[] getvalues() {
+        System.out.println("Գրեք մոտեցման ճշգրտության մոտավոր քանակը սովորաբար 4-7");
+        priblijenyaneriqanaky= sc.nextInt();
+        double[] x1 = new double[matricai_qanak];
+        double[] x2 = new double[matricai_qanak];
+        for (x = 0; x < matricai_qanak; x++) {
+            x1[x] = 0;
+        }
+        for (u = 0; u < priblijenyaneriqanaky; u++) {
+            for (x = 0; x < matricai_qanak; x++) {
+                double gummarum = a[x][matricai_qanak];
+                for (y = 0; y < matricai_qanak; y++) {
+                    if (y != x) {
+                        gummarum -= a[x][y] * x1[y];
+                    }
+                }
+                if (a[x][x] == 0) {
+                    System.out.println(" you have a zero on the main diagonal");;
+                }
+                x2[x] = gummarum / a[x][x];
+            }
+            if (showSteps) {
+                System.out.print("Iteration " + (u + 1) + ": ");
+                for (int i = 0; i < x2.length; i++) {
+                    System.out.print("x" + (i + 1) + "=" + x2[i] + "  ");
+                }
+                System.out.println();
+            }
+            if (stugum(x1, x2)) {
+                break;
+            }
+            for (x = 0; x < matricai_qanak; x++) {
+                x1[x] = x2[x];
+            }
+        }
+        return x2;
+    }
+    private boolean stugum(double[] x1, double[] x2) {
+        for (x = 0; x < matricai_qanak; x++) {
+            if (Math.abs(x2[x] - x1[x]) > eps) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void etveradardzum(double[] x1) {
+        for (x = 0; x < x1.length; x++) {
+            System.out.println("x" + (x + 1) + " = " + x1[x]);
+        }
+        System.out.println();
+    }
+}
